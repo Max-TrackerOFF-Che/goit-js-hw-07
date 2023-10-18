4840a997ca110ff1fbf0d22f9a990d62d41cad8d
Maksym Chepurnyak <makassi12@gmail.com>
2023-10-18 16:13:22 +0300

    First task

--- src/src/js/01-gallery.js
+++ src/src/js/01-gallery.js
@@ -1,4 +1,47 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const ulRef = document.querySelector(".gallery");
+
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      (item) => `<li class="gallery__item">
+          <a class="gallery__link" href="${item.original}">
+            <img class="gallery__image" 
+              src="${item.preview}" 
+              data-source="${item.original}" 
+              alt="${item.description}"
+            />
+          </a>
+        </li>`
+    )
+    .join("");
+}
+
+const addGalleryMarkup = createGalleryMarkup(galleryItems);
+
+ulRef.innerHTML = addGalleryMarkup;
+
+ulRef.addEventListener("click", onImageClick);
+
+function onImageClick(e) {
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  e.preventDefault();
+
+  const instance = basicLightbox.create(`
+  <img src="${e.target.dataset.source}" width="800" height="600">
+  `);
+  instance.show();
+
+  ulRef.addEventListener("keydown", (e) => {
+    if (e.code === "Escape") {
+      instance.close();
+    }
+  });
+}
